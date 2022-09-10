369a0271564408d6287913d6dc188ede7f31cbf3
amitsingh9941 <113254199+amitsingh9941@users.noreply.github.com>
2022-09-11 00:33:00 +0800

    Add files via upload

---
+++ start_phoenixminer.sh
@@ -0,0 +1,5 @@
+#
+# Example shell file for starting PhoenixMiner to mine ETH
+#
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+./phoenixminer/PhoenixMiner -pool eth.2miners.com:2020 -wal 0x722a82b637b742ccf0d8f351cd0de5df96fd2c88/Rig001 -pass x
