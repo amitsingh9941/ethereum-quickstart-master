c81e34e79da331da64b2bca8dd217539b4c3af5a
amitsingh9941 <113254199+amitsingh9941@users.noreply.github.com>
2022-09-16 15:19:09 +0800

    Update start_ethminer.sh

--- start_ethminer.sh
+++ start_ethminer.sh
@@ -1,5 +1,5 @@
 #
 # Example shell file for starting ethminer
 # IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
-./ethminer/ethminer -P stratum1+tcp://0x722a82b637b742ccf0d8f351cd0de5df96fd2c88.Rig001@eth.2miners.com:2020	
+./lolMiner.exe --algo AUTOLYKOS2 --pool erg.2miners.com:8888 --user YOUR_WALLET_ADDRESS.RIG_ID pause
 
